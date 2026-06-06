64715aaaa07ed6327a58ea0941ca5cbe4e45dddf
HoseiniLabs <hoseinilabs.ai@gmail.com>
2026-06-06 23:20:40 +0330

    Add WebView logic and custom Icon

---
+++ App.js
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import { WebView } from 'react-native-webview';
+import { StyleSheet, SafeAreaView, StatusBar } from 'react-native';
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <StatusBar barStyle="dark-content" backgroundColor="#ffffff" />
+      <WebView 
+        source={{ uri: 'https://baravord-admin.vercel.app' }} 
+        style={styles.webview}
+        scalesPageToFit={true}
+        bounces={false}
+      />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ffffff',
+  },
+  webview: {
+    flex: 1,
+  },
+});
